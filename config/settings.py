1e67ae5345cfa914b0a3815b50b91d189e009a8a
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-14 23:49:59 +0200

    modify(settings.py): add new global settings

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,15 @@
-CONSTANT_ONE = 1
-CONSTANT_TWO = 2
+"""
+Global settings for application
+"""
+LANGUAGE = "PL"
+
+
+# STRING CLEANER SETTINGS
+STRING_CLEANER = 'text.StringCleaner'
+STRING_CLEANING_ORDER = [
+    'text.TrimHandler',
+    'text.WhiteSpaceHandler',
+    'text.MultipleSpaceHandler',
+    'text.BoundsHandler',
+    'text.BeforePunctuationHandler',
+]
