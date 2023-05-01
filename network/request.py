9fd7bdeca55ffd9ccd9215116c71317b73023e5a
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-01 22:21:26 +0200

    Revert "feat(request.py): add functionality to subscribe diffrent event"
    This reverts commit 2696d6a400245673b8c64babddc17924498ad853.

--- network/request.py
+++ network/request.py
@@ -2,21 +2,20 @@ from __future__ import annotations
 from typing import List, Text
 import asyncio
 from aiohttp import ClientSession
-from .page import Page
-from .utils import get_random_useragent, url_validator
+from .utils import url_validator, get_random_useragent
+from .webpage import Page
 
 
-class AsyncDownloader:
+class AsyncRequest:
     def __init__(self):
         self.cookies = {"CONSENT": "YES+1"}
         self.loop = asyncio.new_event_loop()
         self.pages = None
+        self.pages_content = None
         self.headers = get_random_useragent()[0]
         asyncio.set_event_loop(self.loop)
 
-    async def download_site(
-        self, session: ClientSession, url: Text, queue
-    ) -> Page:
+    async def download_site(self, session: ClientSession, url: Text) -> Page:
         """
         Downloads a single webpage using the aiohttp library and returns an instance of
         the Page class.
@@ -41,48 +40,44 @@ class AsyncDownloader:
             page.url = url
             page.status_code = response.status
             page.raw_html = await response.text(errors="ignore")
-            queue.put(page)
             return await page.get_page()
 
-    async def download_all_sites(
-        self, sites: List[Text], queue
-    ) -> asyncio.Future[List[Page]]:
+    async def download_all_sites(self, sites: List[Text]) -> asyncio.Future[list]:
         """
-        Downloads multiple webpages using the aiohttp library and returns a future
+        Downloads multiple webpages using the aiohttp library and returns a future 
         object that resolves to a list of instances of the Page class.
 
         Args:
             sites (List[Text]): A list of URLs representing the webpages to download.
 
         Returns:
-            asyncio.Future[list]: A future object that resolves to a list of instances
-            of the Page class, where each instance contains the downloaded webpage's
+            asyncio.Future[list]: A future object that resolves to a list of instances 
+            of the Page class, where each instance contains the downloaded webpage's 
             information, including the URL, HTTP status code, and raw HTML.
         """
         async with ClientSession() as session:
             tasks = []
             for url in sites:
                 if url_validator(url):
-                    task = asyncio.ensure_future(
-                        self.download_site(session, url, queue)
-                    )
+                    task = asyncio.ensure_future(self.download_site(session, url))
                     tasks.append(task)
             return await asyncio.gather(*tasks, return_exceptions=True)
 
-    def fetch(self, sites: List[Text], queue) -> None:
+    def fetch(self, sites: List[Text]) -> AsyncRequest:
         """
-        Downloads multiple webpages using the aiohttp library and returns an instance of
+        Downloads multiple webpages using the aiohttp library and returns an instance of 
         the AsyncRequest class with downloaded webpage information.
 
         Args:
             sites (List[Text]): A list of URLs representing the webpages to download.
 
         Returns:
-            AsyncRequest: An instance of the AsyncRequest class containing the
+            AsyncRequest: An instance of the AsyncRequest class containing the 
             downloaded webpage information.
         """
-        self.pages = self.loop.run_until_complete(self.download_all_sites(sites, queue))
+        self.pages = self.loop.run_until_complete(self.download_all_sites(sites))
         self.loop.stop()
+        return self
 
     def get_pages(self) -> List[Page]:
         return self.pages
