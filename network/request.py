6c6381ae8d60d11c6ba2679b39647b7e56391a00
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-01 22:25:31 +0200

    feat(request.py): add queue to AsyncDownloader

--- network/request.py
+++ network/request.py
@@ -3,10 +3,10 @@ from typing import List, Text
 import asyncio
 from aiohttp import ClientSession
 from .utils import url_validator, get_random_useragent
-from .webpage import Page
+from .page import Page
 
 
-class AsyncRequest:
+class AsyncDownloader:
     def __init__(self):
         self.cookies = {"CONSENT": "YES+1"}
         self.loop = asyncio.new_event_loop()
@@ -15,7 +15,7 @@ class AsyncRequest:
         self.headers = get_random_useragent()[0]
         asyncio.set_event_loop(self.loop)
 
-    async def download_site(self, session: ClientSession, url: Text) -> Page:
+    async def download_site(self, session: ClientSession, url: Text, queue) -> Page:
         """
         Downloads a single webpage using the aiohttp library and returns an instance of
         the Page class.
@@ -40,9 +40,10 @@ class AsyncRequest:
             page.url = url
             page.status_code = response.status
             page.raw_html = await response.text(errors="ignore")
+            queue.put(page)
             return await page.get_page()
 
-    async def download_all_sites(self, sites: List[Text]) -> asyncio.Future[list]:
+    async def download_all_sites(self, sites: List[Text], queue) -> asyncio.Future[list]:
         """
         Downloads multiple webpages using the aiohttp library and returns a future 
         object that resolves to a list of instances of the Page class.
@@ -59,11 +60,11 @@ class AsyncRequest:
             tasks = []
             for url in sites:
                 if url_validator(url):
-                    task = asyncio.ensure_future(self.download_site(session, url))
+                    task = asyncio.ensure_future(self.download_site(session, url, queue))
                     tasks.append(task)
             return await asyncio.gather(*tasks, return_exceptions=True)
 
-    def fetch(self, sites: List[Text]) -> AsyncRequest:
+    def fetch(self, sites: List[Text], queue) -> AsyncDownloader:
         """
         Downloads multiple webpages using the aiohttp library and returns an instance of 
         the AsyncRequest class with downloaded webpage information.
@@ -75,7 +76,7 @@ class AsyncRequest:
             AsyncRequest: An instance of the AsyncRequest class containing the 
             downloaded webpage information.
         """
-        self.pages = self.loop.run_until_complete(self.download_all_sites(sites))
+        self.pages = self.loop.run_until_complete(self.download_all_sites(sites, queue))
         self.loop.stop()
         return self
 
