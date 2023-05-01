82740899f46157a06d28c0957c1bc9984c7acbf9
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-02 00:00:13 +0200

    refactor(app.py): add simple main code

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
-from config import settings
+from webpage_queue import WebPageQueue, Subscriber
+from network.request import AsyncDownloader
 
-print(settings.LANGUAGE)
+
+class TestSub(Subscriber):
+    def update(self, subject, item) -> None:
+        print(f"coś się zadziało w kolejce {item}")
+        
+        
+concrete_sub = TestSub()
+
+queue = WebPageQueue()
+queue.subscribe(concrete_sub, "item_added")
+
+downloader = AsyncDownloader()
+
+url = [
+    "http://quotes.toscrape.com/random",
+    "https://webscraper.io/test-sites/e-commerce/allinone",
+] 
+
+downloader.fetch(url, queue)
+
+print(queue.get())
