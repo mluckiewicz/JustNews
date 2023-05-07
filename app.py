d2b131514928dce1cb35a7986ebb96e4273eab5d
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-07 21:49:54 +0200

    refactor(app.py): refactor main app code to use api interface

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
-from core.webpage_queue.queue import WebPageQueue, Subscriber
-from core.network.downloader import AsyncDownloader
-
-
-class TestSub(Subscriber):
-    def update(self, subject, item) -> None:
-        print(f"coś się zadziało w kolejce {item}")
-        
-        
-concrete_sub = TestSub()
-
-queue = WebPageQueue()
-queue.subscribe(concrete_sub, "item_added")
-
-downloader = AsyncDownloader()
-
-url = [
+from api import JustNews
+urls = [
     "http://quotes.toscrape.com/random",
     "https://webscraper.io/test-sites/e-commerce/allinone",
 ] 
 
-downloader.fetch(url, queue)
-
-print(queue.get())
+jn = JustNews(urls=urls)
+jn.collect_pages()
