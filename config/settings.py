111537b8d4c8bdcd6874cfde4173d2fb31a87398
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-21 22:09:17 +0200

    refactor(settings.py): rename class names in CLEANING_CHIAN

--- config/settings.py
+++ config/settings.py
@@ -16,13 +16,22 @@ COOKIES_CONSENT = {"CONSENT": "YES+1"}
 # STRING CLEANER SETTINGS
 STRING_SANITIZER = 'core.text.sanitizer.StringSanitizer'
 SANITIZATION_ORDER = [
-    'core.text.sanitizer.TrimHandler',
-    'core.text.sanitizer.WhiteSpaceHandler',
-    'core.text.sanitizer.MultipleSpaceHandler',
-    'core.text.sanitizer.BoundsHandler',
-    'core.text.sanitizer.BeforePunctuationHandler',
+    'core.text.text_cleaner.TrimTokens',
+    'core.text.text_cleaner.RemoveWhiteSpaces',
+    'core.text.text_cleaner.RemoveMultipleSpaces',
+    'core.text.text_cleaner.TrimString',
+    'core.text.text_cleaner.RemoveSpacesBeforePunctuation',
 ]
 
 
 # EXTRACTORS
 EXTRACTOR = 'core.extractors.Extractor'
+
+
+# TREE CLEANING ORDER
+CLEANING_CHAIN = [
+    'core.cleaner.node_text_normalizer.NodeTextNormalizer',
+    'core.cleaner.node_text_normalizer.NodeTailNormalizer',
+    'core.cleaner.node_text_normalizer.NodeFlatteningNormalizer',
+    'core.cleaner.node_text_normalizer.TextTailConnNormalizer',
+]
