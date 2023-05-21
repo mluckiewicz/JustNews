0b26c24775141933b5e71b8d9bb759b099ce2695
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-21 22:10:13 +0200

    feature(exceptions.py): add NotImplementedInterface error

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,3 +1,10 @@
 class NotConfigured(Exception):
     "Exception raised when non existing key in settings module is raised"
     pass
+
+
+class NotImplementedInterface(TypeError):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+        
