8d30e279882a3799e479b8221734be412fc939c9
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-27 21:18:11 +0200

    refactor(__init__.py): implement usage of thread pool

--- core/extractors/__init__.py
+++ core/extractors/__init__.py
@@ -1,29 +1,29 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from concurrent.futures import ThreadPoolExecutor
 import threading
-from ..webpage_queue.queue import Subscriber
-from core.text.text_cleaner import clean_string
+from logger import get_logger
+from core.parser.parser import Parser
+from core.extractors.content import ContentExtractor
+from core.webpage_queue.queue import WebPageQueue, Subscriber
+from core.webpage_queue.webpage import WebPage
 
 
 class ExtractorInterface(ABC):
     @abstractmethod
     def extract(self):
         pass
-
+    
 
 class Extractor(Subscriber):
-    def __init__(self) -> None:
-        pass
-
-    def update(self, queue) -> None:
-        while True:
-            page = queue.get()
-            text = " text  "
-            print(
-                f"{threading.current_thread().name} przetwarza: {clean_string(text)}"
-            )
-
-    def run_extraction(self, root):
-        return
-
-    def handle_result(self, future):
-        return
+    def __init__(self, thread_pool: ThreadPoolExecutor, parser: Parser) -> None:
+        self.thread_pool = thread_pool
+        self.parser = parser
+        
+    def update(self, web_queue: WebPageQueue) -> None:
+        self.thread_pool.submit(self.extract, web_queue.get())
+        
+    def extract(self, page: WebPage) -> object:
+        print(f"Przetwarzam element: {page} wątkiem o identyfikatorze: {threading.get_ident()}")
+        dom = self.parser.fromstring(page.raw_html)
+        page.article = ContentExtractor().extract(dom, self.parser)
