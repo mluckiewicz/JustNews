16da9851e11d695a1451cfd8381182cbe37a9a62
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-05-29 22:18:53 +0200

    refactor(__init__.py): move cleaner to extractor and add creation of extractor

--- core/extractors/__init__.py
+++ core/extractors/__init__.py
@@ -5,22 +5,24 @@ from core.parser.parser import Parser
 from core.extractors.content import ContentExtractor
 from core.webpage_queue.queue import WebPageQueue, Subscriber
 from core.webpage_queue.webpage import WebPage
-from core.cleaner.dom_cleaner import DocumentCleaner
+from config import settings
+from config.utils import create_instance
 
 
 class Extractor(Subscriber):
     def __init__(self, thread_pool: ThreadPoolExecutor, parser: Parser) -> None:
         self.thread_pool = thread_pool
         self.parser = parser
-        
+
     def update(self, web_queue: WebPageQueue) -> None:
         self.thread_pool.submit(self.extract, web_queue.get())
-        
+
     def extract(self, page: WebPage) -> object:
-        print(f"Przetwarzam element: {page} wątkiem o identyfikatorze: {threading.get_ident()}")
-        cleaner = DocumentCleaner()
+        print(
+            f"Przetwarzam element: {page} wątkiem o identyfikatorze: {threading.get_ident()}"
+        )
         root = self.parser.fromstring(page.raw_html)
-        cleaned = cleaner.execute(root, self.parser)
-        page.article.content = ContentExtractor().extract(cleaned, self.parser)
-        print(page.article.content)
-        print('\n')
+        content_extractor = create_instance(
+            settings.EXTRACTORS["content_extractor"]["extractor"], root, self.parser
+        )
+        page.article.content = content_extractor.extract()
